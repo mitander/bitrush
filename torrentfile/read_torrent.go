7cdebed4cfbf6b1a9a593ae9e3d0e74859cd51ee
mitander <carlmitander@gmail.com>
2021-04-10 01:00:48 +0200

    add hash function

--- torrentfile/read_torrent.go
+++ torrentfile/read_torrent.go
@@ -1,6 +1,9 @@
 package torrentfile
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
 	"os"
 
 	bencode "github.com/jackpal/bencode-go"
@@ -32,4 +35,31 @@ func Open(path string) (bencodeTorrent, error) {
 	}
 
 	return bct, nil
+}
+
+func (bci *bencodeInfo) hashInfo() ([20]byte, [][20]byte, error) {
+	pieces := []byte(bci.Pieces)
+	hashLen := 20
+	numHashes := len(pieces) / hashLen
+	pieceHashes := make([][20]byte, numHashes)
+
+	if len(pieces)%hashLen != 0 {
+		err := fmt.Errorf("err: fauly pieces length")
+		return [20]byte{}, [][20]byte{}, err
+	}
+
+	for i := range pieceHashes {
+		copy(pieceHashes[i][:], pieces[i*hashLen:(i+1)*hashLen])
+	}
+
+	var info bytes.Buffer
+
+	err := bencode.Marshal(&info, *bci)
+	if err != nil {
+		return [20]byte{}, [][20]byte{}, err
+	}
+	infoHash := sha1.Sum(info.Bytes())
+
+	return infoHash, pieceHashes, nil
+
 }
