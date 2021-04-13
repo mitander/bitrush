1b2ea9dfc988763cd95af58959caf9179cbf0b5c
mitander <carlmitander@gmail.com>
2021-04-13 22:46:15 +0200

    add Serialize and Read to handhshake

--- handshake/handshake.go
+++ handshake/handshake.go
@@ -1,5 +1,10 @@
 package handshake
 
+import (
+	"fmt"
+	"io"
+)
+
 //https://wiki.theory.org/BitTorrentSpecification#Handshake
 type Handshake struct {
 	Pstr     string
@@ -13,4 +18,48 @@ func New(infoHash, peerID [20]byte) *Handshake {
 		InfoHash: infoHash,
 		PeerID:   peerID,
 	}
+}
+
+// handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+func (h *Handshake) Serialize() []byte {
+	buf := make([]byte, len(h.Pstr)+49)
+	buf[0] = byte(len(h.Pstr))
+	curr := 1
+	curr += copy(buf[curr:], h.Pstr)
+	curr += copy(buf[curr:], make([]byte, 8))
+	curr += copy(buf[curr:], h.InfoHash[:])
+	curr += copy(buf[curr:], h.PeerID[:])
+	return buf
+}
+
+func Read(r io.Reader) (*Handshake, error) {
+	var infoHash, peerID [20]byte
+	lengthBuf := make([]byte, 1)
+
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	pstrlen := int(lengthBuf[0])
+	if pstrlen == 0 {
+		err := fmt.Errorf("Invalid pstrlen, cannot be 0")
+		return nil, err
+	}
+
+	hsBuf := make([]byte, 48+pstrlen)
+	_, err = io.ReadFull(r, hsBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	copy(infoHash[:], hsBuf[pstrlen+8:pstrlen+8+20])
+	copy(peerID[:], hsBuf[pstrlen+8+20:])
+
+	hs := Handshake{
+		Pstr:     string(hsBuf[0:pstrlen]),
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+	return &hs, nil
 }
