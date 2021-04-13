bab7cf195a71e95515836d583eeb50df78ac92f7
mitander <carlmitander@gmail.com>
2021-04-13 22:43:36 +0200

    move bitfield stuff to separate package

---
+++ bitfield/bitfield.go
@@ -0,0 +1,52 @@
+package bitfield
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/mitander/bitrush/message"
+)
+
+type Bitfield []byte
+
+func (bf Bitfield) HasPiece(index int) bool {
+	byteIndex := index / 8
+	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+	return bf[byteIndex]>>uint(7-offset)&1 != 0
+}
+
+func (bf Bitfield) SetPiece(index int) {
+	byteIndex := index / 8
+	offset := index % 8
+
+	// silently discard invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+	bf[byteIndex] |= 1 << uint(7-offset)
+}
+
+func recvBitfield(conn net.Conn) (Bitfield, error) {
+	// set deadline to fail instead of blocking after 3 seconds
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg == nil {
+		err := fmt.Errorf("Invalid bitfield data: %s", msg)
+		return nil, err
+	}
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("Invalid msg ID - value: %d expected: 5", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
+}
--- client/client.go
+++ client/client.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/mitander/bitrush/handshake"
-	"github.com/mitander/bitrush/message"
 	"github.com/mitander/bitrush/peers"
 )
 
@@ -19,8 +18,6 @@ type Client struct {
 	infoHash [20]byte
 	peerID   [20]byte
 }
-
-type Bitfield []byte
 
 func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 	// set dial timeout to 3 seconds
@@ -35,7 +32,7 @@ func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 		return nil, err
 	}
 
-	bf, err := recvBitfield(conn)
+	bf, err := bitfield.recvBitfield(conn)
 	if err != nil {
 		conn.Close()
 		return nil, err
@@ -70,45 +67,4 @@ func doHandshake(conn net.Conn, infohash, peerID [20]byte) error {
 		return fmt.Errorf("Invalid infohash recieved: %x - expected: %x", res.InfoHash, infohash)
 	}
 	return nil
-}
-
-func (bf Bitfield) HasPiece(index int) bool {
-	byteIndex := index / 8
-	offset := index % 8
-	if byteIndex < 0 || byteIndex >= len(bf) {
-		return false
-	}
-	return bf[byteIndex]>>uint(7-offset)&1 != 0
-}
-
-func (bf Bitfield) SetPiece(index int) {
-	byteIndex := index / 8
-	offset := index % 8
-
-	// silently discard invalid bounded index
-	if byteIndex < 0 || byteIndex >= len(bf) {
-		return
-	}
-	bf[byteIndex] |= 1 << uint(7-offset)
-}
-
-func recvBitfield(conn net.Conn) (Bitfield, error) {
-	// set deadline to fail instead of blocking after 3 seconds
-	conn.SetDeadline(time.Now().Add(5 * time.Second))
-	defer conn.SetDeadline(time.Time{})
-
-	msg, err := message.Read(conn)
-	if err != nil {
-		return nil, err
-	}
-	if msg == nil {
-		err := fmt.Errorf("Invalid bitfield data: %s", msg)
-		return nil, err
-	}
-	if msg.ID != message.MsgBitfield {
-		err := fmt.Errorf("Invalid msg ID - value: %d expected: 5", msg.ID)
-		return nil, err
-	}
-
-	return msg.Payload, nil
 }
