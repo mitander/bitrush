1e42f4d93f8e3d98c636508ff4bdc0b317c57b52
mitander <carlmitander@gmail.com>
2021-04-22 22:14:25 +0200

    handshake: add test for New

--- handshake/handshake_test.go
+++ handshake/handshake_test.go
@@ -41,12 +41,24 @@ func TestRead(t *testing.T) {
 
 	for _, test := range tests {
 		reader := bytes.NewReader(test.input)
-		m, err := Read(reader)
+		hs, err := Read(reader)
 		if test.fails {
 			assert.NotNil(t, err)
 		} else {
 			assert.Nil(t, err)
 		}
-		assert.Equal(t, test.output, m)
+		assert.Equal(t, test.output, hs)
 	}
+}
+
+func TestNew(t *testing.T) {
+	infoHash := [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116}
+	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	hs := New(infoHash, peerID)
+	expected := &Handshake{
+		Pstr:     "BitTorrent protocol",
+		InfoHash: [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116},
+		PeerID:   [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+	}
+	assert.Equal(t, expected, hs)
 }
