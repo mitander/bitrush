0283a11317a9c2e1382c3a065d049bd2589886e7
mitander <carlmitander@gmail.com>
2021-04-22 22:33:57 +0200

    bitfield: add tests

---
+++ bitfield/bitfield_test.go
@@ -0,0 +1,51 @@
+package bitfield
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHasPiece(t *testing.T) {
+	bf := Bitfield{0b01000110, 0b01010100}
+	outputs := []bool{false, true, false, false, false, true, true, false, false, true, false, true, false, true, false, false, false, false, false, false}
+	for i := 0; i < len(outputs); i++ {
+		t.Log(i, bf.HasPiece(i))
+		assert.Equal(t, outputs[i], bf.HasPiece(i))
+	}
+}
+
+func TestSetPiece(t *testing.T) {
+	tests := []struct {
+		input  Bitfield
+		index  int
+		output Bitfield
+	}{
+		{
+			input:  Bitfield{0b01010110, 0b01010100},
+			index:  4,
+			output: Bitfield{0b01011110, 0b01010100},
+		},
+		{
+			input:  Bitfield{0b01010110, 0b01010100},
+			index:  9,
+			output: Bitfield{0b01010110, 0b01010100},
+		},
+		{
+			input:  Bitfield{0b01010100, 0b01010100},
+			index:  15,
+			output: Bitfield{0b01010100, 0b01010101},
+		},
+		{
+			input:  Bitfield{0b01010110, 0b01110100},
+			index:  18,
+			output: Bitfield{0b01010110, 0b01110100},
+		},
+	}
+	for i, test := range tests {
+		t.Log(i)
+		bf := test.input
+		bf.SetPiece(test.index)
+		assert.Equal(t, test.output, bf)
+	}
+}
