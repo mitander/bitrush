4bcebfb28de9fc94fe0f8039fefc8a1910208490
Carl Mitander <carlmitander@gmail.com>
2021-04-24 00:13:35 +0200

    message: add test for FormatRequestMsg

---
+++ message/message_test.go
@@ -0,0 +1,20 @@
+package message
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatRequestMsg(t *testing.T) {
+	msg := FormatRequestMsg(4, 567, 4321)
+	expected := &Message{
+		ID: MsgRequest,
+		Payload: []byte{
+			0x00, 0x00, 0x00, 0x04,
+			0x00, 0x00, 0x02, 0x37,
+			0x00, 0x00, 0x10, 0xe1,
+		},
+	}
+	assert.Equal(t, expected, msg)
+}
